11b0bcbaba7a54f7b4c27c084553cb555d9cc516
GongXian Cao <gongxiancao@hotmail.com>
2016-10-10 23:19:40 +0800

    use express-ejs-layouts

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 'use strict';
+var expressLayouts = require('express-ejs-layouts');
+
 function lift () {
   var self = this;
   return new Promise(function (resolve) {
-    self.expressApp.set('view engine', framework.config.views.engine);
+    self.expressApp.set('view engine', 'ejs');
+    self.expressApp.use(expressLayouts);
+    self.expressApp.set('layout', framework.config.views.layout);
     resolve();
   });
 }
