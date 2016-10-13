5680419371e234b27689d8886ac5f8708a0be646
GongXian Cao <gongxiancao@hotmail.com>
2016-10-14 01:06:53 +0800

    replace ejs-layout-chain as layout engine

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
-var expressLayouts = require('express-ejs-layouts');
+var ejsLayoutChain = require('ejs-layout-chain');
 
 function lift () {
   var self = this;
   return new Promise(function (resolve) {
     self.expressApp.set('view engine', 'ejs');
-    self.expressApp.use(expressLayouts);
+    self.expressApp.use(ejsLayoutChain);
     self.expressApp.set('layout', framework.config.views.layout);
     resolve();
   });
